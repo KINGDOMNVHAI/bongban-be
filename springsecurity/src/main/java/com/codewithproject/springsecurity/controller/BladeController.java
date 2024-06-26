b522490ca522179d3337af6308dcc842566f4e7e
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-06-26 16:57:12 +0700

    Create blade/upload-image

--- springsecurity/src/main/java/com/codewithproject/springsecurity/controller/BladeController.java
+++ springsecurity/src/main/java/com/codewithproject/springsecurity/controller/BladeController.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -48,5 +51,13 @@ public class BladeController {
     @PostMapping("/public/blade/insert")
     public Map<String,Object> insertBlade(@RequestBody InsertBladeLineRequest req) {
         return bladeServiceImpl.insertBladeLine(req);
+    }
+
+    @PostMapping("/public/blade/upload-image")
+    public Map<String,Object> uploadImageBlade(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        Map<String,Object> result = new HashMap<>();
+        result.put("message", "upload image success");
+        return result;
     }
 }
